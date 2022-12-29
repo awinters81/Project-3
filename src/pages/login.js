19b6fe47a22b961df7324733c526e7abb51dd1e8
podasin <podasin@gmail.com>
2022-12-29 17:08:03 -0500

    form functionality 0001 big headache! benny & pho,(mostly benny)!

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,25 +1,64 @@
 import React, { Component }  from 'react';
-
+import ReactDOM from 'react-dom/client';
 
 // Create function
 const Login = () => {
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    // check if empty then return null
+    // if both are populated 
+    // 
+    // 
+
+  }
+  const handleChange = (e) => {
+    e.preventDefault();
+    
+    const cFrom = e.target.id
+    switch (cFrom) {
+      case 'id-login'       :  
+      if (e.target.value.length > 0) {
+        alert ('filled');
+       } else { 
+        alert('empty')
+      }
+      
+      alert ('left login')
+      var logId = document.getElementById(cFrom);
+
+      
+
+
+      // var logTxt = logId.textContent;
+       console.log(  logId.value);
+       break;
+
+      case 'password-login' :  alert ('left password') 
+
+        break;
+      
+       }
+  
+  }
+
+
   return (
     <div>
       {
    <div className='login-signup-forms'>
    <div className='login-section'>
      <h2 className='page-title'>Login</h2>
-     <form className='form login-form'>
+     <form className='form login-form' onSubmit={handleSubmit}>
        <div className='main-form'>
-         <label for='id-login'>Email:</label>
-         <input className='form-input' type='text' id='id-login' />
+         <label htmlFor='id-login'>Email:</label>
+         <input className='form-input' type='text' id='id-login' onBlur={handleChange} />
        </div>
        <div className='main-form'>
-         <label for='password-login'>Password:</label>
-         <input className='form-input' type='password' id='password-login' />
+         <label htmlFor='password-login'>Password:</label>
+         <input className='form-input' type='password' id='password-login' onBlur={handleChange} />
        </div>
        <div className='main-form'>
-         <button className='main-button' type='submit'>login</button>
+         <button className='main-button' type='submit' >login</button>
        </div>
      </form>
    </div>
@@ -27,15 +66,15 @@ const Login = () => {
      <h2 className='page-title'>Signup</h2>
      <form className='form signup-form'>
        <div className='main-form'>
-         <label for='user-signup'>Username:</label>
+         <label htmlFor='user-signup'>Username:</label>
          <input className='form-input' type='text' id='user-signup' />
        </div>
        <div className='main-form'>
-         <label for='email-signup'>Email:</label>
+         <label htmlFor='email-signup'>Email:</label>
          <input className='form-input' type='text' id='email-signup' />
        </div>
        <div className='main-form'>
-         <label for='password-signup'>Password:</label>
+         <label htmlFor='password-signup'>Password:</label>
          <input className='form-input' type='password' id='password-signup' />
        </div>
        <div className='main-form'>
--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -4,6 +4,7 @@ import React, { Component }  from 'react';
 // Create function
 const Reviews = () => {
 
+
   return (
     <div>
       {
