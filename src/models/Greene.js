85f0e212a028fe7a3ef16b263f93ddaee9a33e40
podasin <podasin@gmail.com>
2023-01-02 22:52:53 -0500

    fix greene model

--- src/models/Greene.js
+++ src/models/Greene.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
+const connection = require('../../config/connection');
+const inventory = require('../seed/items.json');
 
 // 2) -------------------   Construct a new instance of the schema class
 const GreeneSchema = new mongoose.Schema({
-  product: { type: String, required: true },    // Configure individual properties using Schema Types 
+  id: {type: Number, required: true },
+  name: { type: String, required: true },    // Configure individual properties using Schema Types 
   brand: { type: String, required: true },  // The type of data is set to 'String' and required is set to false, meaning it will accept null values
   price: {type: Number, required: true },
-  form: String,  // capsules/liquid.. 
-  quantity: Number,
-  inStock: Boolean,
+  itemcode: String,
+  image: String,  // capsules/liquid.. 
+  inventory: Number,
+  category: String,
   // Use built in date method to get current date
   lastAccessed: { type: Date, default: Date.now },
 });
@@ -17,29 +21,44 @@ const GreeneSchema = new mongoose.Schema({
 const Greene = mongoose.model('Greene', GreeneSchema);
 const handleError = (err) => console.error(err);
 
-//4) ------------------------  Create a New Instance the model, a document
-Greene.create(
-  {
-    product: 'Quercetin',
-    brand: 'Vitamin Shoppe',
-    price: 24.99,
-    form: 'Capsules',
-    quantity: 120,
-    inStock: true,
-  },
-  (err) => (err ? handleError(err) : console.log('Created New Vitamin'))
-);
 
-// Create a New Instance with required product and optional brand properties
-Greene.create(
-  { product: 'Oscillococcinum', 
-    brand: 'Boiron',
-    price: 29.99,
-    form: 'Tube',
-    quantity: 1,
-    inStock: true,
-  },
-    (err) => (err ? handleError(err) : console.log('Created New Vitamin'))
-);
+connection.once('open', async () => {
+  console.log('connected- deleting prior records before seeding');
+  await Greene.deleteMany({});
+
+  console.log('connected- seeding begins');
+  await Greene.insertMany(inventory);
+});
+
+
+// create in bulk---------------------
+// Greene.insertMany(inventory, (err)  =>  (err ? handleError(err) : console.log('Created New Vitamin'))); 
+
+
+
+//4) ------------------------  Create a New Instance the model, a document
+// Greene.create(
+//   {
+//     product: 'Quercetin',
+//     brand: 'Vitamin Shoppe',
+//     price: 24.99,
+//     form: 'Capsules',
+//     quantity: 120,
+//     inStock: true,
+//   },
+//   (err) => (err ? handleError(err) : console.log('Created New Vitamin'))
+// );
+
+// // Create a New Instance with required product and optional brand properties
+// Greene.create(
+//   { product: 'Oscillococcinum', 
+//     brand: 'Boiron',
+//     price: 29.99,
+//     form: 'Tube',
+//     quantity: 1,
+//     inStock: true,
+//   },
+//     (err) => (err ? handleError(err) : console.log('Created New Vitamin'))
+// );
 
 module.exports = Greene;
--- src/models/users.js
+++ src/models/users.js
@@ -1,21 +1,19 @@
 /*  /src/models/Users.js  */
 
 const mongoose = require('mongoose');
-const { Schema, model } = require('mongoose'); //  <==
+const connection = require('../../config/connection');
+// const { Schema, model } = require('mongoose'); //  <==
 
 
 // 2) -------------------   Construct a new instance of the schema class
-const UsersSchema = new mongoose.Schema({
-  email: { type: String, required: true },    // Configure individual properties using Schema Types 
-  password: { type: String, required: true },  // The type of data is set to 'String' and required is set to false, meaning it will accept null values
-  // Use built in date method to get current date
-  lastAccessed: { type: Date, default: Date.now },
-},
+const UsersSchema = new mongoose.Schema(
   {
-    toJSON: {
-        virtuals: true,
-        getters: true
-    },
+    email: { type: String, required: true },    // Configure individual properties using Schema Types 
+    password: { type: String, required: true },  // The type of data is set to 'String' and required is set to false, meaning it will accept null values
+    // Use built in date method to get current date
+    lastAccessed: { type: Date, default: Date.now },
+
+    toJSON: { virtuals: true, getters: true},
     // prevents virtuals from creating duplicate of _id as `id`
     id: false
   }
@@ -23,9 +21,17 @@ const UsersSchema = new mongoose.Schema({
 
 //3) --------------  import/require JSON file to loop through & Seed the database --
 const Users = mongoose.model('Users', UsersSchema);
-// const handleError = (err ? handleError(err) : console.log('New User was created'))
+const handleError = (err ? handleError(err) : console.log('New User was created'))
 
-/*
+
+connection.once('open', async () => {
+  console.log('connected- deleting prior records before seeding');
+  await Users.deleteMany({});
+});
+
+
+
+
 //4) ------------------------  Create a New Instance the model, a document
 Users.create(
     {
@@ -34,7 +40,7 @@ Users.create(
     },
     (err) => (err ? handleError(err) : console.log('Created New User'))
 );
-*/        
+    
   
 module.export = Users;
 
